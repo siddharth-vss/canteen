31b352d4975abab3d5471981cf60d8268f869404
Siddharth <siddharth@vasantsoftwaressolution.com>
2023-10-20 18:23:21 +0530

    blob converter

--- client/src/views/shop/menu.js
+++ client/src/views/shop/menu.js
@@ -6,14 +6,33 @@ const menu = () => {
   const navigate = useNavigate()
   const [Image, setImage] = useState('')
   const [form, setForm] = useState({})
+
+  function fixBinary(bin) {
+    var length = bin.length
+    var buf = new ArrayBuffer(length)
+    var arr = new Uint8Array(buf)
+    for (var i = 0; i < length; i++) {
+      arr[i] = bin.charCodeAt(i)
+    }
+    return buf
+  }
   const converter = (e) => {
     console.log(e)
+
     var render = new FileReader()
     render.readAsDataURL(e.target.files[0])
     render.onloadend = () => {
-      console.log(render.result)
-      setImage(render.result)
+      // console.log(render.result)
+      // setImage(render.result)
+      let arr = render.result.split('data:image/jpeg;base64,')[1]
+
+      var binary = fixBinary(atob(arr))
+      var blob = new Blob([binary], { type: 'image/jpeg' })
+      var url = URL.createObjectURL(blob)
+      console.log(url)
+      setImage(url)
     }
+
     render.onerror = (error) => {
       alert('Error: ', error)
     }
@@ -54,3 +73,61 @@ const menu = () => {
 }
 
 export default menu
+
+/**
+|--------------------------------------------------|
+|         BASE 64 TO BLOB CONVERTER   👇           |
+|--------------------------------------------------|
+*/
+
+/*function fixBinary(bin) {
+      var length = bin.length;
+      var buf = new ArrayBuffer(length);
+      var arr = new Uint8Array(buf);
+      for (var i = 0; i < length; i++) {
+        arr[i] = bin.charCodeAt(i);
+      }
+      return buf;
+    }
+
+    var display = document.getElementById('display');
+    display.innerHTML = (display.innerHTML || '');
+    function log(text) {
+      display.innerHTML += "\n" + text;
+    }
+    var base64 =
+
+
+    var binary = fixBinary(atob(base64));
+    var blob = new Blob([binary], { type: 'image/jpeg' });
+    var url = URL.createObjectURL(blob);
+    log('Created a png blob of size: ' + blob.size);
+    log('Inserting an img...');
+    var img = document.createElement('img');
+    img.src = url;
+    document.body.appendChild(img);
+    log('Blob URL is: ' + url);
+    log('Fetching with ajax...');
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+    xhr.responseType = 'arraybuffer';
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== 4) {
+        return;
+      }
+      log('xhr.status is: ' + xhr.status);
+      log('returned content-type is: ' + xhr.getResponseHeader('Content-Type'));
+      log('returned content-length is: ' + xhr.getResponseHeader('Content-Length'));
+
+      var returnedBlob = new Blob([xhr.response], { type: 'image/png' });
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        var returnedURL = e.target.result;
+        var returnedBase64 = returnedURL.replace(/^[^,]+,/, '');
+        log('xhr.response (in base64) is: ' + returnedBase64);
+        log('is this the expected base64? ' + (returnedBase64 === base64));
+      };
+      reader.readAsDataURL(blob); //Convert the blob from clipboard to base64
+    };
+    xhr.send();*/
